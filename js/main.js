82f241c158cfaec9554cbb71d05da9fe5e831fac
Alex Price <posixoddity@gmail.com>
2015-11-25 09:28:01 -0800

    Added some sample data
    • got Mustache working with sample data
    • added Mustache.js
    • added a sample image for card testing
    • added card expansion code
    • started styling cards

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//spins gear on filter expansion
 $( '.toggle-filters' ).click(function() {
     $('.filters').slideToggle('normal');
     $('.fa-cog').toggleClass('fa-spin');
@@ -5,3 +6,25 @@ $( '.toggle-filters' ).click(function() {
         $('.fa-cog').toggleClass('fa-spin');
     }, 700);
 });
+
+//expands cards on mobile
+$('.card').click(function() {
+    $("#"+this.id).find('.details').slideToggle('normal');
+    $("#"+this.id).find('.upper-share').slideToggle('normal');
+    $("#"+this.id).find('.fa-chevron-down').toggleClass('fa-rotate-180');
+});
+
+/*$(document).ready(function() {
+    $("#templates").load("template.html #cardWrapper",function(){
+    var template = document.getElementById('cardWrapper').innerHTML;
+    for(i=0; i<amtCards; i++) {
+        (function(i) { // protects i in an immediately called function
+            var jqxhr = $.getJSON("https://sheetsu.com/apis/a0252596", function(json)  {
+                cardData = (json.result[i]);
+                checkForCardData();
+                console.log("success in the foor loop");
+            });
+        })(i);
+    }
+});
+*/
